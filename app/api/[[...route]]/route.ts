64534208cb6f686de127fab507ecced44dd00ab7
Tripti Garg <tripti200407garg@gmail.com>
2025-06-22 02:43:54 +0530

    committed

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
@@ -8,14 +9,19 @@ import summary from "./summary";
 
 export const runtime = "edge";
 
-const app = new Hono()
-  .basePath("/api")
+const app= new Hono().basePath("/api")
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
   .route("/transactions", transactions)
   .route("/summary", summary);
 
+
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
+
+export type AppType= typeof routes; 
